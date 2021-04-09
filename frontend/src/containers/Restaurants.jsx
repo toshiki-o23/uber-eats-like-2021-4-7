70d67a1a838fe108fac125470860147a5da7d71d
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-04-09 17:38:38 +0900

    Add useEffect

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { fetchRestaurants } from '../apis/restaurants';
 
 export const Restaurants = () => {
